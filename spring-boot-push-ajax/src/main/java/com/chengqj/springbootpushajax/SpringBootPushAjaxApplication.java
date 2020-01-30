b96aa832aa5e408967181b02376a511d4f17b41d
chengqj <wowcheng163@163.com>
2020-01-30 14:51:21 +0800

    template

---
+++ spring-boot-push-ajax/src/main/java/com/chengqj/springbootpushajax/SpringBootPushAjaxApplication.java
@@ -0,0 +1,13 @@
+package com.chengqj.springbootpushajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPushAjaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPushAjaxApplication.class, args);
+    }
+
+}
