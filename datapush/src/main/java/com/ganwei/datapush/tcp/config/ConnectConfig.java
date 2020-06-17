2ece45dd28b3d53b9fdb4984bdffc0e922e56736
chengqj <wowcheng163@163.com>
2020-06-17 11:37:24 +0800

    update

--- datapush/src/main/java/com/ganwei/datapush/tcp/config/ConnectConfig.java
+++ datapush/src/main/java/com/ganwei/datapush/tcp/config/ConnectConfig.java
@@ -1,16 +1,20 @@
 package com.ganwei.datapush.tcp.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author chengqiujing
  * @Date 2020/6/9 11:10
  * @Desc
  */
+@Component
 @Data
 public class ConnectConfig {
-
+    @Value("{$weijing.conn.ip}")
     private String ip;
 
+    @Value("{$weijing.conn.port}")
     private int port;
 }
--- datapush/src/main/java/com/ganwei/datapush/tcp/config/ReportConfig.java
+++ datapush/src/main/java/com/ganwei/datapush/tcp/config/ReportConfig.java
@@ -1,16 +1,21 @@
 package com.ganwei.datapush.tcp.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author chengqiujing
  * @Date 2020/6/12 14:58
  * @Desc
  */
+@Component
 @Data
 public class ReportConfig {
 
+    @Value("{$weijing.report.buildingNo}")
     private String buildingNo;
 
+    @Value("{$weijing.report.collectorNo}")
     private String collectorNo;
 }
