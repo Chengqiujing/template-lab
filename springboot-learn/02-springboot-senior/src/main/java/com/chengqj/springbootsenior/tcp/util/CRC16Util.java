8a76ca5819d68e1717ababb190c12dd9903fd6ff
chengqj <wowcheng163@163.com>
2020-07-29 20:56:43 +0800

    update

--- springboot-learn/02-springboot-senior/src/main/java/com/chengqj/springbootsenior/tcp/util/CRC16Util.java
+++ springboot-learn/02-springboot-senior/src/main/java/com/chengqj/springbootsenior/tcp/util/CRC16Util.java
@@ -1,5 +1,8 @@
 package com.chengqj.springbootsenior.tcp.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @Author chengqiujing
  * @Date 2020/6/9 14:36
@@ -26,7 +29,7 @@ public class CRC16Util {
     }
 
 
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
 //        String tmp = "testadsfasdfasdfadf1qweqwe2314sdfgsd\"\"";
 //        byte[] bytes = tmp.getBytes();
 //        int crc = getCRC(bytes);
@@ -36,6 +39,31 @@ public class CRC16Util {
 //        bytes1[bytes1.length-2] = (byte)(crc >> 8); // 高位
 //
 //        System.out.println(getCRC(bytes1));
-//    }
+
+//        byte[] bytes = new  byte[2];
+//        System.out.println(0x55 == bytes[0]);
+
+
+        String exp = "ajava1dsfsdjava2python1ajavadsfsdjava3python2";
+        String continueRegEx = "a(java).*?(python)";
+        Pattern continuePattern = Pattern.compile(continueRegEx);
+        Matcher continueMatcher = continuePattern.matcher(exp);
+        while(continueMatcher.find()){
+            System.out.println(continueMatcher.group());
+            System.out.println(continueMatcher.groupCount());
+            String group2 = continueMatcher.group(1);
+            System.out.println(group2);
+            String group = continueMatcher.group(2);
+            System.out.println(group);
+//            String group1 = continueMatcher.group(3);
+//            System.out.println(group1);
+        }
+//        continueMatcher.reset("1231java123");
+//        boolean b = continueMatcher.find();
+//        System.out.println(b);
+//        String groupReset = continueMatcher.group();
+//        System.out.println(groupReset);
+
+    }
 
 }
